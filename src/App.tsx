0a9f58c054334b3de5a0279435e4cf8feed649c4
Maia-Mateus <matteusmaia889@gmail.com>
2023-06-30 05:14:19 -0300

    styled-componets.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-
+import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <div>
-     <h1>Hello world!</h1>
-    </div>
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+      <Button />
+    </>
   )
 }
 
